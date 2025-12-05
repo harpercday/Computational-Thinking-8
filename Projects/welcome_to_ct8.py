0cfd3fa2dee4e66cb6f1eee549167a8cb1663fcf
harpercday <harperday@seattleacademy.org>
2025-12-05 19:26:39 +0000

    test

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("my dogs name is iris")                                  
+print("i have 3 sisters")
+print("i like navy blue")
